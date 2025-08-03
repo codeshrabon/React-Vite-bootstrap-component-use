85e14029e6890231f25b40629a1d96a3e020782e
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-04 03:56:39 +0600

    few syntax uses

--- UseOfBootstrap/src/Components/Home.jsx
+++ UseOfBootstrap/src/Components/Home.jsx
@@ -3,27 +3,36 @@ import React, { useState } from "react";
 function Home() {
   const [count, setCount] = useState(0);
   const [count1, setCount1] = useState(0);
-  
+
   return (
-    <>
+    
 
-      <div className="container text-center">
+    <div className="container text-center">
+        
         <div className="card">
-          <div className="d-flex justify-content-start">
+        <div className="d-flex justify-content-start">
             <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
+            count is {count}
+            
             </button>
-          </div>
         </div>
-      </div>
+        </div>
+        <div className="row justify-content-md center mt-4">
+            <div className="col-lg-2  ">
+                1 of 3
+            </div>
+            <div className="col-md-auto">
+                Variable width content
+            </div>
 
-      <div className="card">
-        <button onClick={() => setCount1((count1) => count1 + 1)}>
-          count - is = {count1}
-        </button>
-      </div>
-    </>
-  );
+        </div>
+        
+        
+        
+        </div>
+        
+
+        );
 }
 
 export default Home;
