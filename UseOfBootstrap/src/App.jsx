3169e9d6f29dcb045e0b7cb498960eb459f02a42
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-05 02:20:35 +0600

    column is not columning

--- UseOfBootstrap/src/App.jsx
+++ UseOfBootstrap/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Components/Home'
+import Home from './Components/Home.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Expriment from './Components/Expriment.jsx'
+
 
 function App() {
 
@@ -13,6 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element = {<Home />}>
         
+        
+        </Route>
+        <Route path='/expriment' element={<Expriment />}>
         </Route>
       </Routes>
       </BrowserRouter>
--- UseOfBootstrap/src/Components/Home.jsx
+++ UseOfBootstrap/src/Components/Home.jsx
@@ -1,38 +1,64 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import React from "react";
 
 function Home() {
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
+const [count, setCount] = useState(0);
+const [count1, setCount1] = useState(0);
 
-  return (
+return (
     
+    <div className="container">
+        {/* <div className="container">
 
-    <div className="container text-center">
-        
-        <div className="card">
-        <div className="d-flex justify-content-start">
-            <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-            
-            </button>
-        </div>
-        </div>
-        <div className="row justify-content-md center mt-4">
-            <div className="col-lg-2  ">
+        </div> */}
+        <div className="text-center">
+        <div className="row">
+            <div className="col col-lg-2">
                 1 of 3
             </div>
             <div className="col-md-auto">
                 Variable width content
             </div>
-
+            <div className="col col-lg-2">
+                3 of 3
+            </div>
         </div>
-        
-        
-        
+        <div className="row">
+            <div className="col">
+                1 of 3
+            </div>
+            <div className="col-md-auto">
+                Variable width content
+            </div>
+            <div className="col col-lg-2">
+                3 of 3
+            </div>
         </div>
-        
+        </div>
+    </div>
+    
+    // <div className="container text-center">
 
-        );
+    //     <div className="card">
+    //     <div className="d-flex justify-content-start">
+    //         <button onClick={() => setCount((count) => count + 1)}>
+    //         count is {count}
+
+    //         </button>
+    //     </div>
+    //     </div>
+    //     <div className="row justify-content-md center mt-4">
+    //         <div className="col-lg-2  ">
+    //             1 of 3
+    //         </div>
+    //         <div className="col-md-auto">
+    //             Variable width content
+    //         </div>
+
+    //     </div>
+
+    //     </div>
+  );
 }
 
 export default Home;
