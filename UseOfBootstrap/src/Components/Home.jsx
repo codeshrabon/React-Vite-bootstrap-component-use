8c60a96880fab2911ea4134b841a77ca1f15472e
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-07 11:36:36 +0600

    working on navBar

--- UseOfBootstrap/src/Components/Home.jsx
+++ UseOfBootstrap/src/Components/Home.jsx
@@ -1,29 +1,75 @@
-import { useState } from "react";
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
 function Home() {
 
-return (
-    
-    <div className="container">
-        <div className="row">
-            <div className="col">
-                Column
-            </div>
-            <div className="col">
-                Column
-            </div>
-            <div className="col">
-                Column
-            </div>
-            <div className="col">
-                Column
-            </div>
-        </div>
+    const navigateHome = useNavigate()
+    const navigateToExpriment = useNavigate()
+    const navigateToImage = useNavigate
 
-        
+    const BackToHome = () => {
+        navigateHome("/")
+    }
+    const goToExpriment = () => {
+        navigateToExperiment("/experiment")
+    }
+return (
+<div className="container">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" >
+          Testing
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={BackToHome}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                
+              <a className="nav-link active" aria-current="page" href="#" >
+                Home
+              </a>
+              
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={goToExpriment}>
+                Experiment
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                ImageShapeSection
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                TextFeatures
+              </a>
+            </li>
+          </ul>
+          <span className="navbar-text">Navbar text with an inline element</span>
+        </div>
+      </div>
+    </nav>
+    <div className="p-3">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam,
+        impedit, quidem ex facilis labore quisquam, quasi odio ut iste voluptate
+        eveniet ratione accusantium nisi dolorum. Quaerat obcaecati itaque
+        asperiores eveniet!
+      </p>
     </div>
+  </div>
 );
 }
 
--- UseOfBootstrap/src/Components/TextFeatures.jsx
+++ UseOfBootstrap/src/Components/TextFeatures.jsx
@@ -1,6 +1,4 @@
-import React from "react"
 import { useNavigate } from "react-router-dom"
-import { Button } from "bootstrap"
 
 
 function TextFeatures(){
